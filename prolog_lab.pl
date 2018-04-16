eff0cf24cac9e74274fe92078caad140ba262215
Nate Tranel <njtranel@gmail.com>
2018-04-15 18:52:06 -0600

    more progress

--- prolog_lab.pl
+++ prolog_lab.pl
@@ -7,21 +7,19 @@ mother(M,C) :- parent(M,C), female(M).
 % rule to determine a father of a child element
 father(F,C) :- parent(F,C), male(F).
 
-% spouse/2
-
-% child/2
-
+spouse(M,F) :- mother(M,C), father(F,C).
+child(C,P) :- parent(P,C).
 son(S,P) :- child(S,P), male(S).
 daughter(D,P) :- child(D,P), female(D).
-% sibling/2
+sibling(X,Y) :- parent(P,X), parent(P,Y), not(X=Y).
 brother(B,O) :- sibling(O,B), male(B).
 sister(S,O) :- sibling(O,S), female(S).
 % uncle/2 (two rules: one by blood, one by marriage.)
 % aunt/2 (two rules: one by blood, one by marriage.)
-% grandparent/2
-% grandfather/2
-% grandmother/2
-% grandchild/2
+grandparent(GP,C) :- parent(GP,P), parent(P,C).
+grandfather(GF,C) :- grandparent(GF,C), male(GF).
+grandmother(GM,C) :- grandparent(GM,C), female(GM).
+grandchild(GC,GP) :- grandparent(GP,GC).
 
 % ancestor/2 (ancestor(X, Y) means X is the ancestor of Y)
 % descendant/2 (descendant(X, Y) means X is the descendant of Y)
