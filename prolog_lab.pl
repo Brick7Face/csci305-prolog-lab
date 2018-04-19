c8be2a54c08a710aba10557a95be575dfc664d67
Nate Tranel <njtranel@gmail.com>
2018-04-19 16:01:01 -0600

    updates some stuff

--- prolog_lab.pl
+++ prolog_lab.pl
@@ -1,35 +1,35 @@
 % Nathanial Tranel
 % CSCI 305 Prolog Lab 1
 
-% rule to determine a mother of a child element
+% rule to ask if Mother was the mother of Child
 mother(Mother,Child) :-
   parent(Mother,Child),
   female(Mother).
 
-% rule to determine a father of a child element
+% rule to ask if Father was the father of Child
 father(Father,Child) :-
   parent(Father,Child),
   male(Father).
 
-% rule to determine if two elements are married
+% rule to ask if Male is married to Female
 spouse(Male,Female) :-
   married(Male,Female).
 
-% rule to determine if the first element is a child of the second
+% rule to ask if Child is the child of Parent
 child(Child,Parent) :-
   parent(Parent,Child).
 
-% rule to determine a son element of the other
+% rule to ask if Son is the son of Parent
 son(Son,Parent) :-
   child(Son,Parent),
   male(Son).
 
-% rule to determine a daughter element of the other
+% rule to ask if Daughter is the daughter of Parent
 daughter(Daughter,Parent) :-
   child(Daughter,Parent),
   female(Daughter).
 
-% rule to determine if two elements are siblings
+% rule to ask if First is the sibling of Second
 sibling(First,Second) :-
   mother(Mother,First),
   mother(Mother,Second),
@@ -37,73 +37,81 @@ sibling(First,Second) :-
   father(Father,Second),
   not(First=Second).
 
-% rule to determine if an element is a brother of the other
+% rule to ask if Brother is a brother of Other
 brother(Brother,Other) :-
   sibling(Other,Brother),
   male(Brother).
 
-% rule to determine if an element is a brother of the other
+% rule to ask if Sister is a sister of Other
 sister(Sister,Other) :-
   sibling(Other,Sister),
   female(Sister).
 
-% rule to determine if element is an uncle of a child
+% rule to ask if Uncle is a blood uncle of Child
 uncle(Uncle,Child) :-
   sibling(Uncle,Parent),
   parent(Parent,Child),
   male(Uncle).
 
-% rule for uncle by marriage (check if it is an uncle of the spouse)
+% rule to ask if Uncle is an uncle-by-marriage of Child
 uncle(Uncle,Child) :-
-  uncle(Uncle,spouse(Child)).
+  aunt(Aunt,Child),
+  spouse(Uncle,Aunt).
 
-% rule to determine if element is an aunt of a child
+% rule to ask if Aunt is a blood aunt of Child
 aunt(Aunt,Child) :-
   sibling(Aunt,Parent),
   parent(Parent,Child),
   female(Aunt).
 
-% rule for aunt by marriage (check if it is an uncle of the spouse)
+% rule to ask if Aunt is an aunt-by-marriage of Child
 aunt(Aunt,Child) :-
-  aunt(Aunt,spouse(Child)).
+  uncle(Uncle,Child),
+  spouse(Aunt,Uncle).
 
-% rule to determine is an element is a grandparent of the other
+% rule to ask if Grandparent is a grandparent of Child
 grandparent(Grandparent,Child) :-
   parent(Grandparent,Parent),
   parent(Parent,Child).
 
-% rule to determine if an element is a grandfather of the other
+% rule to ask if Grandfather is a grandfather of Child
 grandfather(Grandfather,Child) :-
   grandparent(Grandfather,Child),
   male(Grandfather).
 
-% rule to determine if an element is a grandmother of the other
+% rule to ask if Grandmother is a grandmother of Child
 grandmother(Grandmother,Child) :-
   grandparent(Grandmother,Child),
   female(Grandmother).
 
-% rule to determine is an element is a grandchild of the other
+% rule to ask if Grandchild is a grandchild of Grandparent
 grandchild(Grandchild,Grandparent) :-
   grandparent(Grandparent,Grandchild).
 
 % ancestor/2 (ancestor(X, Y) means X is the ancestor of Y)
 % descendant/2 (descendant(X, Y) means X is the descendant of Y)
 
-% rule to determine if an element is older than another
+% rule to ask if Older is in fact older than Younger
 older(Older,Younger) :-
   born(Older,YearO),
   born(Younger,YearY),
   YearO < YearY.
 
-% rule to determine if an element is younger than another
+% rule to ask if Younger is younger than Older
 younger(Younger,Older) :-
   born(Older,YearO),
   born(Younger,YearY),
   YearO > YearY.
 
-% regentWhenBorn/2
+% rule to ask who was King or Queen (Regent) when Person was born
+regentWhenBorn(Regent,Person) :-
+    reigned(Regent,Start,End),
+    born(Person,Birth),
+    (Start =< Birth),
+    (Birth =< End),
+    not(Regent=Person).
 
-% rule to determine if an element is a first cousin of the other
+% rule to ask if Cousin is a cousin to Other
 cousin(Cousin,Other) :-
   parent(Parent,Cousin),
   parent(ParentTwo,Other),
